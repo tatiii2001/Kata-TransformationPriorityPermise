f1b6410e3fec566098202938517f668d2902c854
tatiii2001 <tatiiigomez2001@gmail.com>
2024-04-02 10:34:00 +0100

    Refactoring

--- src/main/kotlin/com/kata/ObtainUppercase.kt
+++ src/main/kotlin/com/kata/ObtainUppercase.kt
@@ -4,7 +4,7 @@ class ObtainUppercase {
     fun indexOfUppercase(word: String): List<Int> {
         val listOfIndexes : MutableList<Int> = mutableListOf()
         val chars = word.toCharArray()
-        println(chars)
+
         for ((index, char) in chars.withIndex()){
             if(char.isUpperCase()){
                 listOfIndexes.add(index)
